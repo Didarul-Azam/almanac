1c45ae6b5b25512ed614ee6fdd0785c33364ac00
Mehedi24434 <mehedi24434@gmail.com>
2023-09-13 13:43:41 +0600

    str 3 done

---
+++ almanac/analysis/position_sereies_variable_risk.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from almanac.utils.standardDeviation import standardDeviation
+from almanac.config.configs import *
+
+def calculate_position_series_given_variable_risk(
+    capital: float,
+    risk_target_tau: float,
+    fx: pd.Series,
+    multiplier: float,
+    instrument_risk: standardDeviation,
+) -> pd.Series:
+
+    # N = (Capital × τ) ÷ (Multiplier × Price × FX × σ %)
+    ## resolves to N = (Capital × τ) ÷ (Multiplier × FX × daily stdev price terms × 16)
+    ## for simplicity we use the daily risk in price terms, even if we calculated annualised % returns
+    daily_risk_price_terms = instrument_risk.daily_risk_price_terms()
+
+    return (
+        capital
+        * risk_target_tau
+        / (multiplier * fx * daily_risk_price_terms * (BUSINESS_DAYS_IN_YEAR ** 0.5))
+    )
