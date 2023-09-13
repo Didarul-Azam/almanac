4c8f4b37d4a94c059abfdc3ed4976253c25fe17f
ornob <ornob39@gmail.com>
2023-09-13 12:21:34 +0600

    strategy 1 class

--- build/lib/almanac/config/configs.py
+++ build/lib/almanac/config/configs.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from scipy.stats import norm
+import pandas as pd
+import numpy as np
 
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 Frequency = Enum("Frequency", "Natural Year Month Week BDay")
@@ -14,3 +17,7 @@ MONTHS_PER_YEAR = 12
 SECONDS_PER_YEAR = 365.25 * 24 * 60 * 60
 
 PERIODS_PER_YEAR = {MONTH: MONTHS_PER_YEAR, WEEK: WEEKS_PER_YEAR, YEAR: 1}
+
+QUANT_PERCENTILE_EXTREME = 0.01
+QUANT_PERCENTILE_STD = 0.3
+NORMAL_DISTR_RATIO = norm.ppf(QUANT_PERCENTILE_EXTREME) / norm.ppf(QUANT_PERCENTILE_STD)
