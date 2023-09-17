157aa78b0b5049cb3d51d0d255c4673f01452d71
Mehedi24434 <mehedi24434@gmail.com>
2023-09-17 14:58:13 +0600

    str 9 continuation

---
+++ almanac/analysis/positions_9.py
@@ -0,0 +1,220 @@
+import pandas as pd
+from almanac.utils.standardDeviation import standardDeviation
+from almanac.config.configs import *
+from almanac.analysis.forecasts import ewmac
+from copy import copy
+
+
+def calculate_position_series_given_fixed_risk(capital: float,
+                                               risk_target_tau: float,
+                                               current_price: pd.Series,
+                                               fx: pd.Series,
+                                               multiplier: float,
+                                               instrument_risk_ann_perc: float) -> pd.Series:
+
+    # N = (Capital × τ) ÷ (Multiplier × Price × FX × σ %)
+    position_in_contracts = capital * risk_target_tau / \
+        (multiplier * current_price * fx * instrument_risk_ann_perc)
+
+    return position_in_contracts
+
+
+def calculate_position_series_given_variable_risk(
+    capital: float,
+    risk_target_tau: float,
+    fx: pd.Series,
+    multiplier: float,
+    instrument_risk: standardDeviation,
+) -> pd.Series:
+
+    # N = (Capital × τ) ÷ (Multiplier × Price × FX × σ %)
+    # resolves to N = (Capital × τ) ÷ (Multiplier × FX × daily stdev price terms × 16)
+    # for simplicity we use the daily risk in price terms, even if we calculated annualised % returns
+    daily_risk_price_terms = instrument_risk.daily_risk_price_terms()
+
+    return (
+        capital
+        * risk_target_tau
+        / (multiplier * fx * daily_risk_price_terms * (BUSINESS_DAYS_IN_YEAR ** 0.5))
+    )
+
+
+def calculate_position_series_given_variable_risk_for_dict(
+    capital: float,
+    risk_target_tau: float,
+    idm: float,
+    weights: dict,
+    fx_series_dict: dict,
+    multipliers: dict,
+    std_dev_dict: dict,
+) -> dict:
+
+    position_series_dict = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_series_given_variable_risk(
+                    capital=capital * idm * weights[instrument_code],
+                    risk_target_tau=risk_target_tau,
+                    multiplier=multipliers[instrument_code],
+                    fx=fx_series_dict[instrument_code],
+                    instrument_risk=std_dev_dict[instrument_code],
+                ),
+            )
+            for instrument_code in std_dev_dict.keys()
+        ]
+    )
+
+    return position_series_dict
+
+
+def calculate_position_dict_with_trend_filter_applied(
+    adjusted_prices_dict: dict,
+    average_position_contracts_dict: dict,
+) -> dict:
+
+    list_of_instruments = list(adjusted_prices_dict.keys())
+    position_dict_with_trend_filter = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_with_trend_filter_applied(
+                    adjusted_prices_dict[instrument_code],
+                    average_position_contracts_dict[instrument_code],
+                ),
+            )
+            for instrument_code in list_of_instruments
+        ]
+    )
+
+    return position_dict_with_trend_filter
+
+
+def calculate_position_with_trend_filter_applied(
+    adjusted_price: pd.Series, average_position: pd.Series
+) -> pd.Series:
+
+    filtered_position = copy(average_position)
+    ewmac_values = ewmac(adjusted_price)
+    bearish = ewmac_values < 0
+    filtered_position[bearish] = 0
+
+    return filtered_position
+
+
+def calculate_position_dict_with_symmetric_trend_filter_applied(
+    adjusted_prices_dict: dict,
+    average_position_contracts_dict: dict,
+) -> dict:
+
+    list_of_instruments = list(adjusted_prices_dict.keys())
+    position_dict_with_trend_filter = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_with_symmetric_trend_filter_applied(
+                    adjusted_prices_dict[instrument_code],
+                    average_position_contracts_dict[instrument_code],
+                ),
+            )
+            for instrument_code in list_of_instruments
+        ]
+    )
+
+    return position_dict_with_trend_filter
+
+
+def calculate_position_with_symmetric_trend_filter_applied(
+    adjusted_price: pd.Series, average_position: pd.Series
+) -> pd.Series:
+
+    filtered_position = copy(average_position)
+    ewmac_values = ewmac(adjusted_price)
+    bearish = ewmac_values < 0
+    filtered_position[bearish] = -filtered_position[bearish]
+
+    return filtered_position
+
+
+def calculate_position_dict_with_trend_forecast_applied(
+    adjusted_prices_dict: dict,
+    average_position_contracts_dict: dict,
+    std_dev_dict: dict,
+    fast_span: int = 64,
+) -> dict:
+
+    list_of_instruments = list(adjusted_prices_dict.keys())
+    position_dict_with_trend_filter = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_with_trend_forecast_applied(
+                    adjusted_prices_dict[instrument_code],
+                    average_position_contracts_dict[instrument_code],
+                    stdev_ann_perc=std_dev_dict[instrument_code],
+                    fast_span=fast_span,
+                ),
+            )
+            for instrument_code in list_of_instruments
+        ]
+    )
+
+    return position_dict_with_trend_filter
+
+
+def calculate_position_with_trend_forecast_applied(
+    adjusted_price: pd.Series,
+    average_position: pd.Series,
+    stdev_ann_perc: standardDeviation,
+    fast_span: int = 64,
+) -> pd.Series:
+    from almanac.analysis.forecasts import calculate_forecast_for_ewmac
+    forecast = calculate_forecast_for_ewmac(
+        adjusted_price=adjusted_price,
+        stdev_ann_perc=stdev_ann_perc,
+        fast_span=fast_span,
+    )
+
+    return forecast * average_position / 10
+
+
+def calculate_position_dict_with_multiple_trend_forecast_applied(
+    adjusted_prices_dict: dict,
+    average_position_contracts_dict: dict,
+    std_dev_dict: dict,
+    fast_spans: list,
+) -> dict:
+
+    list_of_instruments = list(adjusted_prices_dict.keys())
+    position_dict_with_trend_filter = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_with_multiple_trend_forecast_applied(
+                    adjusted_prices_dict[instrument_code],
+                    average_position_contracts_dict[instrument_code],
+                    stdev_ann_perc=std_dev_dict[instrument_code],
+                    fast_spans=fast_spans,
+                ),
+            )
+            for instrument_code in list_of_instruments
+        ]
+    )
+
+    return position_dict_with_trend_filter
+
+
+def calculate_position_with_multiple_trend_forecast_applied(
+    adjusted_price: pd.Series,
+    average_position: pd.Series,
+    stdev_ann_perc: standardDeviation,
+    fast_spans: list,
+) -> pd.Series:
+    from almanac.analysis.forecasts_9 import calculate_combined_ewmac_forecast
+    forecast = calculate_combined_ewmac_forecast(
+        adjusted_price=adjusted_price,
+        stdev_ann_perc=stdev_ann_perc,
+        fast_spans=fast_spans,
+    )
+
+    return forecast * average_position / 10
