1a15d3d8accf0afbe607b2cbdc1032df97e2fe08
ornob <ornob39@gmail.com>
2023-09-17 13:05:08 +0600

    strategy 7 class + prev return bug fixed

--- almanac/strategy/strategy5.py
+++ almanac/strategy/strategy5.py
@@ -1,6 +1,6 @@
 from almanac.data.data import get_data_dict
 from almanac.analysis.positions import calculate_position_series_given_variable_risk_for_dict, calculate_position_dict_with_trend_filter_applied
-from almanac.analysis.calculate_returns import aggregate_returns
+from almanac.analysis.calculate_returns import aggregate_returns, calculate_perc_returns_for_dict_with_costs
 from almanac.strategy.baseStrategy import StrategyBase
 from typing import Union
 
@@ -53,14 +53,26 @@ class Strategy5(StrategyBase):
         )
         return position_contracts_dict
 
+    def calculate_returns(self):
+        returns_dict = calculate_perc_returns_for_dict_with_costs(
+            position_contracts_dict=self.position_contracts_dict,
+            fx_series=self.fx_series_dict,
+            multipliers=self.multipliers,
+            capital=self.capital,
+            adjusted_prices=self.adjusted_prices,
+            cost_per_contract_dict=self.cost_per_contract_dict,
+            std_dev_dict=self.std_dev_dict
+        )
+
+        return returns_dict
+
     def run_strategy(self):
-        adjusted_prices, current_prices = self.get_data()
-        self.fx_series_dict = self.create_fx_series(adjusted_prices)
+        self.adjusted_prices, self.current_prices = self.get_data()
+        self.fx_series_dict = self.create_fx_series(self.adjusted_prices)
         self.std_dev_dict = self.calculate_std_dev(
-            adjusted_prices, current_prices)
+            self.adjusted_prices, self.current_prices)
         self.position_contracts_dict = self.calculate_positions(
-            adjusted_prices, self.std_dev_dict)
-        self.perc_return_dict = self.calculate_returns(
-            self.position_contracts_dict, adjusted_prices)
+            self.adjusted_prices, self.std_dev_dict)
+        self.perc_return_dict = self.calculate_returns()
         self.perc_return_agg = aggregate_returns(self.perc_return_dict)
         self.calculate_quantstats(self.perc_return_agg)
